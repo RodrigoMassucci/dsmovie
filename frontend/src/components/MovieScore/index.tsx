50ff3f85a2f9d1c06e84d85ba35c5bd8b9f0c40f
RodrigoMassucci <rodrigo.massucci@outlook.com>
2022-01-14 14:41:19 -0300

    Props

--- frontend/src/components/MovieScore/index.tsx
+++ frontend/src/components/MovieScore/index.tsx
@@ -3,7 +3,7 @@ import './styles.css';
 
 function MovieScore() {
     const score = 3.5;
-    const count = 15;
+    const count = 13;
 
     return (
         <div className="dsmovie-score-container" >
